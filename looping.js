1fed3d5760b9069d767d87976e55e71c56c218f4
Aneathaa <159550441+AneathaaRse@users.noreply.github.com>
2024-05-06 12:42:55 +0530

    Update looping.js

--- looping.js
+++ looping.js
@@ -57,6 +57,45 @@ var data = {
         }
       }
 
+//forEach loop
+
+  Object.keys(data).forEach(key => {
+    if (Array.isArray(data[key])) {
+      console.log(`${key}:`);
+      data[key].forEach(item => console.log(`- ${item}`));
+    } else if (typeof data[key] === 'object') {
+      console.log(`${key}:`);
+      Object.keys(data[key]).forEach(innerKey => {
+        console.log(`- ${innerKey}: ${data[key][innerKey]}`);
+      });
+    } else {
+      console.log(`${key}: ${data[key]}`);
+    }
+  });
+
+  //forof loop
+
+  for (let skill of data.skills) {
+    console.log(skill);
+  }
+  
+  // Using for...of with object keys
+  for (let key of Object.keys(data)) {
+    if (Array.isArray(data[key])) {
+      console.log(`${key}:`);
+      for (let item of data[key]) {
+        console.log(`- ${item}`);
+      }
+    } else if (typeof data[key] === 'object') {
+      console.log(`${key}:`);
+      for (let innerKey of Object.keys(data[key])) {
+        console.log(`- ${innerKey}: ${data[key][innerKey]}`);
+      }
+    } else {
+      console.log(`${key}: ${data[key]}`);
+    }
+  }
+
 
     
    
